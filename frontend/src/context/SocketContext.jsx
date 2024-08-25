9804b36ea7c9775d4eb105f01f24331c4e0aceac
Rishabh Singh <rishabhsingh812002@gmail.com>
2024-08-25 13:56:16 +0530

    Changes made in SocketContext

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("https://mav-chat-app.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
